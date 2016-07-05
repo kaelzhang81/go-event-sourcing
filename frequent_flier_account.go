1b254a2b45e96c508189c683ee23595ea125cc0f
kaelzhang81 <zhangy1981@qq.com>
2016-07-05 19:56:32 +0800

    add FrequentFlierAccount

--- frequent_flier_account.go
+++ frequent_flier_account.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+)
+
+// FrequentFlierAccount represents the state of an instance of the frequent flier
+// account aggregate. It tracks changes on itself in the form of domain events.
+type FrequentFlierAccount struct {
+	id         string
+	miles      int
+	tierPoints int
+	status     Status
+}
+
+func (account FrequentFlierAccount) String() string {
+	format := `FrequentFlierAccount: %s
+    Miles: %d
+    TierPoints: %d
+    Status: %s`
+
+	return fmt.Sprintf(format, account.id, account.miles, account.tierPoints, account.status)
+}
