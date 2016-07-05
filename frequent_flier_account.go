4e71f756e7895fd468d1f5357b63339a05861ff2
kaelzhang81 <zhangy1981@qq.com>
2016-07-05 21:05:26 +0800

    refactor

--- frequent_flier_account.go
+++ frequent_flier_account.go
@@ -7,17 +7,20 @@ import (
 // FrequentFlierAccount represents the state of an instance of the frequent flier
 // account aggregate. It tracks changes on itself in the form of domain events.
 type FrequentFlierAccount struct {
-	id         string
-	miles      int
-	tierPoints int
-	status     Status
+	id              string
+	miles           int
+	tierPoints      int
+	status          Status
+	expectedVersion int
+	changes         []interface{}
 }
 
 func (account FrequentFlierAccount) String() string {
 	format := `FrequentFlierAccount: %s
     Miles: %d
     TierPoints: %d
-    Status: %s`
+    Status: %d
+`
 
 	return fmt.Sprintf(format, account.id, account.miles, account.tierPoints, account.status)
 }
@@ -26,21 +29,26 @@ func NewFrequentFlierAccountFromHistory(events []interface{}) *FrequentFlierAcco
 	state := &FrequentFlierAccount{}
 
 	for _, event := range events {
-		switch e := event.(type) {
-		case FrequentFlierAccountCreated:
-			state.id = e.AccountId
-			state.miles = e.OpeningMiles
-			state.tierPoints = e.OpeningTierPoints
-			state.status = StatusRed
-		case StatusMatched:
-			state.status = e.NewStatus
-		case FlightTaken:
-			state.miles = e.MilesAdded
-			state.tierPoints = e.TierPointAdded
-		case PromotedToGoldStatus:
-			state.status = StatusGold
-		}
+		state.transition(event)
+		state.expectedVersion++
 	}
 
 	return state
+}
+
+func (state *FrequentFlierAccount) transition(event interface{}) {
+	switch e := event.(type) {
+	case FrequentFlierAccountCreated:
+		state.id = e.AccountId
+		state.miles = e.OpeningMiles
+		state.tierPoints = e.OpeningTierPoints
+		state.status = StatusRed
+	case StatusMatched:
+		state.status = e.NewStatus
+	case FlightTaken:
+		state.miles += e.MilesAdded
+		state.tierPoints += e.TierPointAdded
+	case PromotedToGoldStatus:
+		state.status = StatusGold
+	}
 }
