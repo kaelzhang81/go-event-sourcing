dabdab997d4df1c38d75b5ad4cc534b03bbf0e16
kaelzhang81 <zhangy1981@qq.com>
2016-07-05 19:25:55 +0800

    add structs

--- frequent_flier_events.go
+++ frequent_flier_events.go
@@ -1,1 +1,26 @@
 package main
+
+type Status int
+
+const (
+	StatusRed    Status = itoa
+	StatusSilver Status = itoa
+	StatusGold   Status = itoa
+)
+
+type FrequentFlierAccountCreated struct {
+	AccountId        string
+	OpeningMiles     int
+	OpeningTierPoint int
+}
+
+type StatusMatched struct {
+	NewStatus Status
+}
+
+type FlightTaken struct {
+	MilesAdded     int
+	TierPointAdded int
+}
+
+type PromotedToGoldStatus struct{}
